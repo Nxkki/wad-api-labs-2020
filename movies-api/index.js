c01213436e9ce070e0c0d0b3444cd90af0d69699
Nikki Osita <nikkio358@yahoo.com>
2021-01-01 16:45:09 +0000

    Initial Express App

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import moviesRouter from './api/movies';
+
 
 dotenv.config();
 
@@ -8,6 +10,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use('/api/movies', moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
