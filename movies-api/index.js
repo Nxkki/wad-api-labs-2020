3511b76ef45142c29c29474b2084b605c5a30e21
Nikki Osita <nikkio358@yahoo.com>
2021-01-02 00:51:18 +0000

    Topic 10 - Salt and Hash

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import postsRouter from './api/movies';
+
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers} from './seedData'
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
 import session from 'express-session';
 import authenticate from './authenticate';
-
 
 dotenv.config();
 
@@ -23,13 +24,12 @@ const errHandler = (err, req, res, next) => {
 
 const app = express();
 
-const port = process.env.PORT;
-
 if (process.env.SEED_DB) {
   loadUsers();
 }
 
-//session middleware
+const port = process.env.PORT;
+
 app.use(session({
   secret: 'ilikecake',
   resave: true,
@@ -39,68 +39,13 @@ app.use(session({
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.static('public'));
-//update /api/Movie route
+
 app.use('/api/movies', authenticate, moviesRouter);
+app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
-//Users router
 app.use('/api/users', usersRouter);
-//Genres Router
-app.use('/api/genres', genresRouter);
+app.use('/api/genres', genresRouter)
 app.use(errHandler);
-
-
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
-// import dotenv from 'dotenv';
-// import express from 'express';
-// import moviesRouter from './api/movies';
-// import postsRouter from './api/movies';
-
-// import bodyParser from 'body-parser';
-// import './db';
-// import {loadUsers} from './seedData'
-// import usersRouter from './api/users';
-// import genresRouter from './api/genres';
-// import session from 'express-session';
-// import authenticate from './authenticate';
-
-// dotenv.config();
-
-// const errHandler = (err, req, res, next) => {
-//   /* if the error in development then send stack trace to display whole error,
-//   if it's in production then just send error message  */
-//   if(process.env.NODE_ENV === 'production') {
-//     return res.status(500).send(`Something went wrong!`);
-//   }
-//   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
-// };
-
-// const app = express();
-
-// if (process.env.SEED_DB) {
-//   loadUsers();
-// }
-
-// const port = process.env.PORT;
-
-// app.use(session({
-//   secret: 'ilikecake',
-//   resave: true,
-//   saveUninitialized: true
-// }));
-
-// //configure body-parser
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
-
-// app.use('/api/movies', authenticate, moviesRouter);
-// app.use(express.static('public'));
-// app.use('/api/movies', moviesRouter);
-// app.use('/api/users', usersRouter);
-// app.use('/api/genres', genresRouter)
-// app.use(errHandler);
-// app.listen(port, () => {
-//   console.info(`Server running at ${port}`);
-// });
